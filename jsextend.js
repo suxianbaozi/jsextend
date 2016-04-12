f2b0951942946c23a95b55f6753f5cd169c5b406
suxianbaozi <12qiushi@163.com>
2016-04-12 18:23:21 +0800

    Update jsextend.js

--- jsextend.js
+++ jsextend.js
@@ -1,10 +1,16 @@
+/**
+ * 用来继承函数内部的属性和方法
+ */
+ 
 function getParent(instance,parent,arg){  
     instance.extend = parent;  
     instance.extend.apply(instance,arg);//传递参数  
     instance.extend = null;  
     delete instance.extend;  
 }  
-  
+/**
+ * 用来继承prototype
+ */
 Function.prototype.extend = function(parent){  
     this.prototype = parent.prototype;  
 };  
